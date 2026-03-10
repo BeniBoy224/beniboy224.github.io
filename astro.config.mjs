6625497bf59686ddf1b0b4711af2177e43c92343
Ben Thompson <cowissad@gmail.com>
2026-03-10 00:20:59 +0000

    added github deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://beniboy224.github.io',
+  base: '/beniboy224.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
