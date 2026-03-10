82ea4da685339092f0d1c6ea14d518fe5cd62d84
Ben Thompson <cowissad@gmail.com>
2026-03-10 00:31:49 +0000

    changes to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://beniboy224.github.io',
-  base: '/beniboy224.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
